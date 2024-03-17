69ced133f1c864a6371bd37c1e4a4e1e45911b0f
Clink123 <clink123gamer@gmail.com>
2024-03-17 00:03:42 -0400

    Implement ox lib notify

--- custom_scripts/client/notification.lua
+++ custom_scripts/client/notification.lua
@@ -4,5 +4,16 @@ function Utils.CustomScripts.notify(type,message)
 	-- You can change your notification here. There are 4 notifications types: 'success', 'error', 'warning' and 'info'.
 	-- If you've set the config to "other", you need to configure your notification export.
 	-- Remove the error line below after implementing.
-	error("^3The function for the selected notification script in the Config has not been implemented: ^1" .. Config.custom_scripts_compatibility.notification .. "^3. If you're not using any of the pre-built notification scripts, you'll need to implement it here.^7")
+	--error("^3The function for the selected notification script in the Config has not been implemented: ^1" .. Config.custom_scripts_compatibility.notification .. "^3. If you're not using any of the pre-built notification scripts, you'll need to implement it here.^7")
+  local typeStr = type
+  
+  if type == "info" then
+    type = "inform"
+  end
+  
+  lib.notify({
+      title = type:gsub("^%l", string.upper),
+      description = message,
+      type = typeStr
+  })
 end
